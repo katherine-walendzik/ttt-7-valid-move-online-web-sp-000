cf67114474268da808f196f4115e1e7f638457f3
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-11 20:29:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,27 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(input) 
+  index = input.to_i - 1
+end
 
-# code your #valid_move? method here
+def move(board, index, value = "X")
+  board[index] = value
+end
 
+def valid_move?
+  
+end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else board == "X" || board == "O"
+    return true
+  end
+end
